08d4722584a2a78027e2590f300816b5fe450fb1
thirdlongitude <yvetteh2000@gmail.com>
2021-01-18 23:07:50 +0800

    enable all 10

--- run_all_trivia.py
+++ run_all_trivia.py
@@ -7,15 +7,15 @@ from selenium import webdriver
 import trivia
 
 driver = webdriver.Chrome()
-# trivia.w101_spells(driver)
-# trivia.w101_wiz_city(driver)
-# trivia.w101_adventuring(driver)
-# trivia.w101_conjuring(driver)
-# trivia.w101_magical(driver)
-# trivia.w101_mystical(driver)
-# trivia.w101_spellbinding(driver)
-# trivia.greek_myth(driver)
-# trivia.world_capitals(driver)
+trivia.w101_spells(driver)
+trivia.w101_wiz_city(driver)
+trivia.w101_adventuring(driver)
+trivia.w101_conjuring(driver)
+trivia.w101_magical(driver)
+trivia.w101_mystical(driver)
+trivia.w101_spellbinding(driver)
+trivia.greek_myth(driver)
+trivia.world_capitals(driver)
 trivia.book_quotes(driver)
 
 # ask for user input to quit webdriver session
